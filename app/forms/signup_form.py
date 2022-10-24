5bf2a550a548badc0a8dba4008ab14ff602c9de3
Mariel <marielvaca@gmail.com>
2022-10-24 18:02:28 -0500

    fixed email validation error

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -24,5 +24,5 @@ class SignUpForm(FlaskForm):
     first_name = StringField(
         'first name', validators=[DataRequired()])
     last_name  = StringField('last name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists, Email])
+    email = StringField('email', validators=[DataRequired(), user_exists, Email()])
     password = StringField('password', validators=[DataRequired()])
