e05e1f3fd20f6fa40e2d80c2c397876cd0e6287f
Mariel <marielvaca@gmail.com>
2022-10-27 15:37:49 -0500

    edits to profile component

--- react-app/src/components/Profile/ProfilePage.js
+++ react-app/src/components/Profile/ProfilePage.js
@@ -1,3 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import {}
+import {getProfileFeed} from "../../store/stories";
+
+function userProfile(){
+    const dispatch = useDispatch();
+    const userFeed = useSelector((state) => Object.values(state.storyState.feed));
+
+    useEffect()
+    return (
+        <div className="user-profile-page">
+
+        </div>
+    );
+};
+export default userProfile;
