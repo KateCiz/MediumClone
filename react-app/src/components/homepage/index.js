d32086127bad04f9d7b7cf3c812262458d355f74
FenneAustin <austindfenne@gmail.com>
2022-11-02 23:26:30 -0700

    added homepage

--- react-app/src/components/homepage/index.js
+++ react-app/src/components/homepage/index.js
@@ -2,14 +2,24 @@ import React, {useState} from 'react';
 import {useSelector} from 'react-redux';
 import UserStoryFeed from '../Feed/UserStoryFeed';
 import AllStoriesFeed from '../Feed/AllStoriesFeed';
+import HomeBanner from "./HomeBanner"
 
 const HomePage = () => {
     const sessionUser = useSelector((state) => state.session.user);
 
     return (
         <div>
-            {sessionUser && <UserStoryFeed user={sessionUser}/>}
-            {!sessionUser && <AllStoriesFeed />}
+            {sessionUser &&
+            (
+            <UserStoryFeed user={sessionUser}/>
+            )}
+            {!sessionUser &&
+            (
+            <div>
+                <HomeBanner />
+            <AllStoriesFeed />
+            </div>
+            )}
         </div>
     )
 }
--- react-app/src/components/navbar/NavBar.js
+++ react-app/src/components/navbar/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom'
 import HorizontalNavBar from "./HorizontalNavBar/HorizontalNavBar"
 import VerticalNavBar from './VerticalNavBar/VerticalNavBar';
+import MainNavBar from './MainPageNavBar/index'
+import WriteStoryNavBar from './WriteStoryNavBar/index';
 
 const NavBar = () => {
-
+  const history = useHistory()
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
     <>
-    { sessionUser ? (<VerticalNavBar user={sessionUser}/>) : (<HorizontalNavBar />)}
+    {console.log(history.location.pathname)}
+    {console.log(!sessionUser)}
+    {console.log(history.location.path ==='/')}
+    { !sessionUser && history.location.path == "/" && <MainNavBar /> }
+    { sessionUser && history.location.path === '/' ? (<VerticalNavBar user={sessionUser}/>) : null}
     </>
   );
 }
